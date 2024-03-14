949e83a6471c7ddb252b2e0dd5dab9f2bd6fef90
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-14 12:44:51 +0530

    Add files via upload

--- MyClass.java
+++ MyClass.java
@@ -1,48 +1,35 @@
 public class MyClass {
-    // Static variables...
-    private static int staticVar1;
-    private static String staticVar2;
-
-    // Instance variables...
+    // Instance variables
     private int instanceVar1;
     private String instanceVar2;
 
-    // Static methods
-    public static void staticMethod1() {
-        System.out.println("Static Method 1");
+    // Constructor
+    public MyClass(int instanceVar1, String instanceVar2) {
+        this.instanceVar1 = instanceVar1;
+        this.instanceVar2 = instanceVar2;
     }
 
-    public static void staticMethod2() {
-        System.out.println("Static Method 2");
-    }
-
-    // Instance methods
-    public void instanceMethod1() {
-        System.out.println("Instance Method 1");
-    }
-
-    public void instanceMethod2() {
-        System.out.println("Instance Method 2");
-    }
-
-    public static void main(String[] args) {
-        // Accessing static variables and methods
-        staticVar1 = 10;
-        staticVar2 = "Hello";
-        System.out.println("Static Variable 1: " + staticVar1);
-        System.out.println("Static Variable 2: " + staticVar2);
-        staticMethod1();
-        staticMethod2();
-
-        // Creating an instance of MyClass
-        MyClass obj = new MyClass();
-
-        // Accessing instance variables and methods
-        obj.instanceVar1 = 20;
-        obj.instanceVar2 = "World";
+    // Static method with instance passed as argument
+    public static void printInstanceVariables(MyClass obj) {
         System.out.println("Instance Variable 1: " + obj.instanceVar1);
         System.out.println("Instance Variable 2: " + obj.instanceVar2);
-        obj.instanceMethod1();
-        obj.instanceMethod2();
+    }
+
+    // Static method with instance created inside the method
+    public static void printInstanceVariables() {
+        MyClass obj = new MyClass(42, "Hello");
+        System.out.println("Instance Variable 1: " + obj.instanceVar1);
+        System.out.println("Instance Variable 2: " + obj.instanceVar2);
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        MyClass obj1 = new MyClass(10, "World");
+
+        // Calling static method with instance passed as argument
+        printInstanceVariables(obj1);
+
+        // Calling static method creating an instance inside the method
+        printInstanceVariables();
     }
 }
